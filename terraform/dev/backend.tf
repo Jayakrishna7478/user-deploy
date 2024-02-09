b05b82d8cc879f477dc87ce1429283cc183eafb5
Jayakrishna7478 <jayakrishnachowdry123@gmail.com>
2024-02-09 18:55:36 +0530

    user-deploy

---
+++ terraform/dev/backend.tf
@@ -0,0 +1,4 @@
+bucket = "aws-state-dev"
+key    = "user"
+region = "us-east-1"
+dynamodb_table = "aws-locking-dev"
---
+++ terraform/prod/backend.tf
@@ -0,0 +1,4 @@
+bucket = "aws-state-prod"
+key    = "user"
+region = "us-east-1"
+dynamodb_table = "aws-locking-prod"
